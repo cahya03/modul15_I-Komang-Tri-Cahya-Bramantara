bf4adeeab2a041ff8bc6da6093e75a63a8f0f6f4
cahya03 <69002518+cahya03@users.noreply.github.com>
2022-11-07 20:58:17 +0700

    formatted file with prettier

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import{BrowserRouter,Routes, Route} from 'react-router-dom'
-import {Main} from './components/Main'
-import { Details } from './components/Details';
-import { Search } from './components/Search';
-import { SearchBar } from './components/SearchBar';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Main } from "./components/Main";
+import { Details } from "./components/Details";
+import { Search } from "./components/Search";
+import { SearchBar } from "./components/SearchBar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />}/>
-        <Route path='/details/:isbn' element={<Details />}/>
-        <Route path="/search" element={<SearchBar/>}/>
-        <Route path="/search/:q" element={<Search/>}/>
+        <Route path="/" element={<Main />} />
+        <Route path="/details/:isbn" element={<Details />} />
+        <Route path="/search" element={<SearchBar />} />
+        <Route path="/search/:q" element={<Search />} />
       </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
 
 export default App;
